ecba2f8d7cd9c650ffe60f6d3818ad7544326686
sandipsky <sandipshakya75@gmail.com>
2024-02-19 15:26:51 +0545

    project created

--- projects/ngx-nepali-datepicker/src/public-api.ts
+++ projects/ngx-nepali-datepicker/src/public-api.ts
@@ -1,6 +1,13 @@
 /*
  * Public API Surface of ngx-nepali-datepicker
  */
-
-export * from './lib/ngx-nepali-datepicker.service';
+export * from './lib/services/nepali-datepicker-angular.service';
 export * from './lib/ngx-nepali-datepicker.component';
+export * from './lib/ngx-nepali-datepicker.module';
+
+/*
+ * Public API Surface of nepali-datepicker-angular
+ */
+
+
+
